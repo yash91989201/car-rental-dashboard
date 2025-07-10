451c09507565a9e2b47f1f9c88dbaa8b36bb9e97
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-07-10 16:55:04 +0530

    feat: redirect to login or dashboard in the root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
+import type { GetServerSideProps } from "next";
+import { getServerSession } from "next-auth";
 import Head from "next/head";
+import { authOptions } from "./api/auth/[...nextauth]";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (session == null) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+    };
+  }
+
+  return {
+    redirect: {
+      permanent: false,
+      destination: "/dashboard",
+    },
+  };
+};
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Car Rental Dashboard</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className=""></main>
-    </>
+    <Head>
+      <title>Car Rental Dashboard</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
   );
 }
